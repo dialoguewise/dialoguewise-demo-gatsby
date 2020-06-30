2fcf061bd1c94a9e86edc59e53e81c1ef7ecadf2
Ken Thomas <ken.tms@gmail.com>
2020-06-30 16:01:27 +1000

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,20 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [    
+    {
+      resolve: `gatsby-source-dialoguewise`,
+      options: {
+        apiKey: 'b1266377591c4f2a9494c3abdd2cac5381D6Z825D26CEBAE8B6rn',
+        emailHash: '/kgmM46s1xC56BOFWRZp4j+0bdU19URpXdNT9liAX50=',
+        dialogues: [
+            {
+                slug: 'hero-section',
+                isPilot: false,
+                variableList: {
+                    '@wheel': 2
+                },
+            },
+        ],
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function DwDemo({ data }) {
+  return (
+    <div>
+      <h1>DialogueWise Demo</h1>
+
+      {data.allDialogueWise.edges.map(({ node }) => (
+        JSON.parse(node.content).map( (content, index) => (
+            <div key={index} dangerouslySetInnerHTML={{ __html: content['hero-content'] }} />)
+        )
+      ))}
+    </div>
+  )
 }
+
+export const query = graphql`
+query {
+  allDialogueWise(filter: {slug: {eq: "hero-section"}}) {
+    edges {
+      node {
+        content
+        slug
+        error
+      }
+    }
+  }
+}
+`
